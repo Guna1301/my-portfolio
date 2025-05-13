2577c7e0be6ef91258489c0f9d2066d58c38fb0b
Guna1301 <gskotipalli2005@gmail.com>
2025-05-13 18:14:40 +0530

    contact page done

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,20 @@ import Hero from './sections/Hero'
 import About from './sections/About'
 import Projects from './sections/Projects'
 import Experiences from './sections/Experiences'
+import Contact from './sections/Contact'
+import Footer from './sections/Footer'
 
 function App() {
   return (
     <div className='container mx-auto max-w-7xl'>
       <Navbar/>
       <Hero/>
-      <About/>
+      <About/> 
       <Projects/>
       <Experiences/>
-      <section className='min-h-screen'/>
-      <section className='min-h-screen'/>
-      <section className='min-h-screen'/>
-      <section className='min-h-screen'/>
-      <section className='min-h-screen'/>
-    </div>
+      <Contact/>
+      <Footer/>
+    </div> 
   )
 }
 
---
+++ src/sections/Footer.jsx
@@ -0,0 +1,21 @@
+
+const Footer = () => {
+  return (
+    <section className="felx flex-wrap items-center
+    justify-between gap-5 pb-3 text-sm text-neutral-400 c-space">
+        <div
+            className="mb-4 bg-gradient-to-r from-transparent via-neutral-700 to-transparent
+            h-[1px] w-full"
+        />
+        <div>
+            <p></p>
+            <p>|</p>
+            <p></p>
+        </div>
+
+        
+    </section>
+  )
+}
+
+export default Footer
